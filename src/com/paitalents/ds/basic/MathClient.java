0e22e95d28b2334eea0f206be5daf1e9ff11ef9a
ramapaital <62502070+ramapaital@users.noreply.github.com>
2021-07-03 22:22:17 +0530

    Update MathClient.java
    useMathFunction

--- src/com/paitalents/ds/basic/MathClient.java
+++ src/com/paitalents/ds/basic/MathClient.java
@@ -4,7 +4,15 @@ public class MathClient {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to basic DS problem");
-
+		useMathFunction();
 	}
-
+	
+	private static void useMathFunction() {
+		//1. Use Sin function
+		//2. Cos 
+		//3. Use min, median, mode, max, mean
+		System.out.println("Math.PI =  " + Math.PI);
+		int x = 12, y = 32;
+		System.out.println("Max of " + x + " , " + y + " is " + Math.max(x, y));
+	}
 }
